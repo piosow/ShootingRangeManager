69b7873b2ca8133dc4364ec734e32630f56485cf
piosow <peter.sowiak@gmail.com>
2018-03-11 09:44:43 +0100

    Update frmMain.cs

--- ShootingRangeManager/frmMain.cs
+++ ShootingRangeManager/frmMain.cs
@@ -51,6 +51,35 @@ namespace ShootingRangeManager
                 MessageBox.Show("ASDASDASD");
             }
         }
+        
+        private void a()
+        {
+            throw new Exception("Tralalalalalallla", ex);
+        }
+        
+        private void b()
+        {
+            try
+            {
+                a();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Forward błędu", ex);
+            }
+        }
+        
+        private void c()
+        {
+            try
+            {
+                b();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Jeszcze jeden forward błędu", ex);
+            }
+        }
 
         private void btnAddShooter_Click(object sender, EventArgs e)
         {
@@ -58,6 +87,7 @@ namespace ShootingRangeManager
             {
                 //CustomMessage customMessage = new CustomMessage();
                 //customMessage.ShowDialog();
+                c();
 
             }
             catch (Exception ex)
